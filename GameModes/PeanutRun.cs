2290ec5155735b9d32fbaeb3b7d8471cb0e7ded1
Gage Cottrell <inquisitorpinkie@gmail.com>
2023-12-17 15:27:23 -0700

    change peanutRun to allow the fight to continue after the first escape

--- GameModes/PeanutRun.cs
+++ GameModes/PeanutRun.cs
@@ -5,23 +5,50 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using PlayerEvent = Exiled.Events.Handlers.Player;
-using MapEvent = Exiled.Events.Handlers.Map;
-using Scp914Handler = Exiled.Events.Handlers.Scp914;
+using ServerEvent = Exiled.Events.Handlers.Server;
 using Exiled.API.Features;
 using MEC;
+using Exiled.Events.EventArgs.Interfaces;
+using UnityEngine;
 
 namespace CustomGameModes.GameModes
 {
     internal class PeanutRun : IGameMode
     {
+        CoroutineHandle roundLoop;
 
         public void OnRoundStart()
         {
-            PlayerEvent.TriggeringTesla += OnTriggeringTesla;
+            PlayerEvent.TriggeringTesla += DeniableEvent;
             PlayerEvent.Escaping += OnEscape;
             PlayerEvent.Dying += OnDied;
-            PlayerEvent.SearchingPickup += OnPickup;
+            PlayerEvent.SearchingPickup += DeniableEvent;
+            ServerEvent.RespawningTeam += DeniableEvent;
 
+            roundLoop = Timing.RunCoroutine(_roundLoop());
+        }
+        public void OnRoundEnd()
+        {
+            PlayerEvent.TriggeringTesla -= DeniableEvent;
+            PlayerEvent.Escaping -= OnEscape;
+            PlayerEvent.Dying -= OnDied;
+            PlayerEvent.SearchingPickup -= DeniableEvent;
+            ServerEvent.RespawningTeam -= DeniableEvent;
+
+            if (roundLoop.IsRunning)
+                Timing.KillCoroutines(roundLoop);
+        }
+
+        public void OnWaitingForPlayers()
+        {
+            OnRoundEnd();
+        }
+
+        //-----------------------------------------------------------------------------------------------------------------------------------
+        //-----------------------------------------------------------------------------------------------------------------------------------
+
+        private IEnumerator<float> _roundLoop()
+        {
             var players = Player.List.ToList();
             players.ShuffleList();
 
@@ -30,38 +57,80 @@ namespace CustomGameModes.GameModes
                 Action<Player> setup = i switch
                 {
                     0 => SetupSCP173,
-                    1 => SetupSCP173,
+                    1 => players.Count > 3 ? SetupSCP173 : SetupClassD,
                     _ => SetupClassD,
                 };
 
                 setup(players[i]);
             }
-        }
-        public void OnRoundEnd()
-        {
-            PlayerEvent.TriggeringTesla -= OnTriggeringTesla;
-            PlayerEvent.Escaping -= OnEscape;
-            PlayerEvent.Dying -= OnDied;
-            PlayerEvent.SearchingPickup -= OnPickup;
-        }
 
-        public void OnWaitingForPlayers()
-        {
-            OnRoundEnd();
-        }
-
-
-        public void SetupSCP173(Player player)
-        {
-            player.Role.Set(PlayerRoles.RoleTypeId.Scp173, PlayerRoles.RoleSpawnFlags.UseSpawnpoint);
-            Timing.CallDelayed(15, () =>
+            while (true)
             {
-                player.ShowHint("""
-                    Tesla Gates are OFF!
-                    If Any Class-D escape,
-                    <b>You Die Immediately!</b>
-                    """, 15);
-            });
+
+            PeanutHUD:
+                {
+                    var humans = Player.Get(p => p.IsHuman).ToList();
+                    var inLcz = humans.Where(p => p.Zone == Exiled.API.Enums.ZoneType.LightContainment).Count();
+                    var inHcz = humans.Where(p => p.Zone == Exiled.API.Enums.ZoneType.HeavyContainment).Count();
+                    var inEz = humans.Where(p => p.Zone == Exiled.API.Enums.ZoneType.Entrance).Count();
+                    var inSurface = humans.Where(p => p.Zone == Exiled.API.Enums.ZoneType.Surface).Count();
+
+                    IEnumerable<string> lines()
+                    {
+                        if (inLcz > 0) yield return $"<color=orange>LCZ: {inLcz}</color>";
+                        if (inHcz > 0) yield return $"<color=yellow>HCZ: {inHcz}</color>";
+                        if (inEz > 0) yield return $"<color=green>Entrance: {inEz}</color>";
+                        if (inSurface > 0) yield return $"<color=blue>Surface: {inSurface}</color>";
+                    }
+
+                    var message = string.Join(" | ", lines());
+
+                    foreach (var scp in Player.Get(p => p.Role.Team == PlayerRoles.Team.SCPs))
+                    {
+                        scp.Broadcast(2, $"""
+                            Where to Find the Class-Ds:
+                            {message}
+                            """, shouldClearPrevious: true);
+                    }
+                }
+
+            ChaosHUD:
+                {
+                    var chaosPlayers = Player.Get(p => p.Role.Team == PlayerRoles.Team.ChaosInsurgency).ToList();
+
+                    if (chaosPlayers.Count > 0)
+                    {
+                        var scpCount = Player.Get(p => p.IsScp).Count();
+                        var cdCount = Player.Get(p => p.Role.Type == PlayerRoles.RoleTypeId.ClassD).Count();
+                        var scpMsg = $"<color=red>SCPs</color>: {scpCount}";
+                        var cdMsg = $"<color=orange>Class-D</color>: {cdCount}";
+                        foreach (var ci in chaosPlayers)
+                        {
+                            ci.Broadcast(2, $"{scpMsg} - {cdMsg}", shouldClearPrevious: true);
+                        }
+                    }
+                }
+
+                yield return Timing.WaitForSeconds(1);
+            }
+        }
+
+        //-----------------------------------------------------------------------------------------------------------------------------------
+        //-----------------------------------------------------------------------------------------------------------------------------------
+
+        public void SetupSCP173(Player scp)
+        {
+            scp.Role.Set(PlayerRoles.RoleTypeId.Scp173, PlayerRoles.RoleSpawnFlags.UseSpawnpoint);
+
+            if (Round.EscapedDClasses > 0)
+            {
+                PrepareSCPAfterEscape(scp);
+                Timing.CallDelayed(15, () => ShowEscapedMessage(scp));
+            }
+            else
+            {
+                Timing.CallDelayed(15, () => ShowSCPStartupMessage(scp));
+            }
         }
 
         public void SetupClassD(Player player)
@@ -71,25 +140,55 @@ namespace CustomGameModes.GameModes
             player.CurrentItem = player.AddItem(ItemType.KeycardO5);
             player.AddItem(ItemType.SCP207, 4);
 
-            Timing.CallDelayed(15, () =>
-            {
-                player.ShowHint("""
-                    No Picking up Items!
-                    Tesla Gates are OFF!
-                    <b>Just Run and Escape!</b>
-                    """, 15);
-            });
+            Timing.CallDelayed(15, () => ShowClassDStartupMessage(player));
         }
 
+        //-----------------------------------------------------------------------------------------------------------------------------------
+        //-----------------------------------------------------------------------------------------------------------------------------------
 
-        public void OnTriggeringTesla(TriggeringTeslaEventArgs ev)
+        public void PrepareSCPAfterEscape(Player scp)
+        {
+            scp.MaxHealth = 1;
+            scp.Health = 1;
+            scp.MaxArtificialHealth = 0;
+        }
+
+        public void ShowEscapedMessage(Player scp)
+        {
+            scp.ShowHint("""
+                A Class-D has Escaped and returned as Chaos Insurgency!
+                Beware!
+                """, 20);
+        }
+
+        public void ShowClassDStartupMessage(Player classd)
+        {
+            classd.ShowHint("""
+                No Picking up Items!
+                Tesla Gates are OFF!
+                <b>Just Run and Escape!</b>
+                """, 15);
+        }
+
+        public void ShowSCPStartupMessage(Player scp)
+        {
+            scp.ShowHint("""
+                Tesla Gates are OFF!
+                Kill the <b><color=orange>Class-D</color></b> Before They Escape!
+                """, 15);
+        }
+
+        //-----------------------------------------------------------------------------------------------------------------------------------
+        //-----------------------------------------------------------------------------------------------------------------------------------
+
+        public void DeniableEvent(IDeniableEvent ev)
         {
             ev.IsAllowed = false;
         }
 
         public void OnDied(DyingEventArgs ev)
         {
-            if (ev.Player.Role.Type == PlayerRoles.RoleTypeId.ClassD)
+            if (ev.Player.IsHuman)
             {
                 if (ev.Attacker?.Role.Type == PlayerRoles.RoleTypeId.Scp173)
                 {
@@ -106,13 +205,9 @@ namespace CustomGameModes.GameModes
         {
             foreach (var scp in Player.List.Where(p => p.Role.Type == PlayerRoles.RoleTypeId.Scp173))
             {
-                scp.Explode();
+                PrepareSCPAfterEscape(scp);
+                ShowEscapedMessage(scp);
             }
-        }
-
-        public void OnPickup(SearchingPickupEventArgs e)
-        {
-            e.IsAllowed = false;
         }
     }
 }
